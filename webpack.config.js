573eaa2fc5b78fbddd19e42cf3a4a7535c9e18db
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-04-28 11:22:29 +0300

    webpack cofiguration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Output directory
+        filename: 'bundle.js', // Output bundle file name
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Match JavaScript files
+                exclude: /node_modules/, // Exclude node_modules directory
+                use: {
+                    loader: 'babel-loader', // Use babel-loader for JavaScript files
+                },
+            },
+            {
+                test: /\.css$/, // Match CSS files
+                use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // Path to your HTML template
+            filename: 'index.html', // Output HTML file name
+        }),
+    ],
+};
