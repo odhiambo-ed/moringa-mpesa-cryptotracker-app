32527dd394b3443055f5f1bae87631d1377db3a4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-04-28 11:40:45 +0300

    add image loaders configurations

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,37 @@ module.exports = {
             },
             {
                 test: /\.css$/, // Match CSS files
-                use: ['style-loader', 'css-loader', 'eslint-loader'], // Use style-loader and css-loader for CSS files
+                use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i, // Match image files
+                use: [
+                    {
+                        loader: 'url-loader', // Use url-loader for image files
+                        options: {
+                            limit: 8192, // Convert images smaller than 8kb to base64 strings
+                            name: '[name].[hash:7].[ext]', // File name pattern
+                            outputPath: 'images', // Output directory for images
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i, // Match font files
+                use: [
+                    {
+                        loader: 'file-loader', // Use file-loader for font files
+                        options: {
+                            name: '[name].[hash:7].[ext]', // File name pattern
+                            outputPath: 'fonts', // Output directory for fonts
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.js$/, // Match JavaScript files
+                exclude: /node_modules/, // Exclude node_modules directory
+                use: ['eslint-loader'], // Use eslint-loader for linting
             },
         ],
     },
